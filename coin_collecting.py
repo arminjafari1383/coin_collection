07e787a74e1a84bf03e07a4dce268718bca6d292
armin <arminjafri138386@gmail.com>
2024-07-17 10:46:28 +0330

    start create a option controller

--- coin_collecting.py
+++ coin_collecting.py
@@ -23,6 +23,59 @@ player_speed = 10
 
 obstacle_width = 100
 obstacle_height = 20
-obstacle_x = random.randint(0,screen_width)
-obstacle_y = screen_height - player_height - 10
-obstacle_speed = 10
+obstacle_x = random.randint(0,screen_width - obstacle_width)
+obstacle_y = -obstacle_height
+obstacle_speed = 3
+obstacle_speed_increase = 0.2
+
+coin_radius = 15
+obstacle_height = 20
+coin_x = random.randint(coin_radius,screen_width - coin_radius)
+coin_y = -coin_radius
+coin_speed = 4
+coin_speed_increase = 0.2
+
+clock = pygame.time.Clock
+score = 0
+
+normal_font = pygame.font.Font(None,40)
+game_over_font = pygame.font.Font(None,80)
+
+dark_mode = True
+
+running = True
+game_over = False
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and game_over:
+            if event.key == pygame.K_RETURN:
+                game_over = False
+                player_x = screen_width // 2 - player_width // 2
+                player_y = screen_height - player_height - 10
+                obstacle_x = random.randint(0 , screen_width - obstacle_width)
+                obstacle_y = -obstacle_height
+                obstacle_speed = 3
+                coin_x = random.randint(coin_radius, screen_width - coin_radius)
+                coin_y = -coin_radius
+                coin_speed = 4
+                score = 0
+    if not game_over:
+        keys = pygame.key.get_pressed()
+        print(keys)
+        if keys[pygame.K_LEFT] and player_x > 0:
+            player_x -= player_speed
+        if keys[pygame.K_RIGHT] and player_x < screen_width-player_width:
+            player_x += player_speed
+
+        obstacle_y += obstacle_speed
+        if obstacle_y > screen_height:
+            obstacle_x = random.randint(0 , screen_width - obstacle_width)
+            obstacle_y = -obstacle_height
+    pygame.display.update()
+    clock.tick(60)
+
+
+pygame.quit()
